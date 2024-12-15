cf1d454c4dbf2f9c4a26bcd4b476d4ac03b7f6d4
b2112 <viacheslav.martson@gmail.com>
2024-12-15 17:57:21 +0200

    lesson no 1

---
+++ l no 3.py
@@ -0,0 +1,70 @@
+#1015
+#
+# seconds=int(input())
+# print(seconds//60)
+
+
+#1016
+# a,b = map(int, input().split())
+# print(a - (a //b) * b)
+#
+
+#1017
+
+# x=int(input())
+# if x%2 == 0:
+#     print("Yes")
+# else:
+#     print("No")
+
+#1018
+# a,b = map(int, input().split())
+# if a > b:
+#     print(a*2)
+# else:
+#     print(b*2)
+
+
+#1019
+# a=int(input())
+# if a % 2 == 0:
+#     print(int(a / 2))
+# else:
+#     print(0)
+
+#1020
+
+# a,b = map(int, input().split())
+# if b > a:
+#     print(a,b)
+# else:
+#     print(b,a)
+
+
+# #1021
+# p = float(input())
+# if  p % 1 == 0: print("Yes")
+# else: print("No")
+
+
+# #1022
+# a,b,c,d = map(int, input().split())
+# min,max = 0,0
+# if a>b and a>c and a>d:
+#     max = a
+# elif b>a and b>c and b>d:
+#     max = b
+# elif c>a and c>b and c>d:
+#     max = c
+# else: max = d
+#
+# if a<b and a<c and a<d:
+#     min = a
+# elif b<a and b<c and b<d:
+#     min = b
+# elif c<a and c<b and c<d:
+#     min = c
+# else: min = d
+# print(min, max)
+
+
