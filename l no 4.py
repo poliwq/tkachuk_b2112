3d357e1cf0f2df99397adf449234b532955e4c41
b2112 <viacheslav.martson@gmail.com>
2024-12-22 17:51:13 +0200

    lesson no 6

--- l no 4.py
+++ l no 4.py
@@ -33,24 +33,33 @@
 # for i in lst_mult_square:
 #     print(i)
 
-
-# from random import randint
-# lst = [randint(-10, 10) for i in range(20)]
-# print(lst)
-#
-# sum_negative = 0
-#
-# for num in lst:
-#     if num < 0: sum_negative += num
-# print("sum negative item list: ", sum_negative)
-
 from random import randint
 lst = [randint(-10, 10) for i in range(20)]
 print(lst)
 
-sum_of_even = 0
-for num in lst:
-    if
+sum_negative = 0
 
+for num in lst:
+    if num < 0: sum_negative += num
+print("sum negative item list: ", sum_negative)
+
+from random import randint
+sum_elements_mult_3 = 0
+for i in range(len(lst)):
+    print(lst[i])
+    if i % 3 == 0:
+        sum_elements_mult_3 += lst[i]
+print("sum elements that are multiples of 3: ", sum_elements_mult_3)
+
+mult_range = 1
+min_value_list = min(lst)
+max_value_list = max(lst)
+index_min = lst.index(min_value_list)
+index_max = lst.index(max_value_list)
+if index_max < index_min:
+    index_max, index_min = index_min, index_max
+for i in range (index_min, index_max, 1):
+    mult_range *= lst[1]
+print("elements between minimum and maximum, mult range is ")
 
 
